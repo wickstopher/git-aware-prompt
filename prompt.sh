88c0651587dc5e172abe4daf0b0c60d5b5db0482
Christopher R. Wicks <cwicks@datto.com>
2016-07-07 13:08:35 -0400

    More prompt changes.

--- prompt.sh
+++ prompt.sh
@@ -5,7 +5,7 @@ find_git_branch() {
     if [[ "$branch" == "HEAD" ]]; then
       branch='detached*'
     fi
-    git_branch="[$branch]"
+    git_branch="[$branch] "
   else
     git_branch=""
   fi
@@ -14,7 +14,8 @@ find_git_branch() {
 find_git_dirty() {
   local status=$(git status --porcelain 2> /dev/null)
   if [[ "$status" != "" ]]; then
-    git_dirty='*'
+    git_dirty='* '
+    git_branch=${git_branch% }
   else
     git_dirty=''
   fi
